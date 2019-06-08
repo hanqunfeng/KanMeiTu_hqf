03a1432835c58cce9def6a4b008428574aa25003
hanqf <hanqf2008@163.com>
2019-06-08 18:12:15 +0800

    添加依赖包，修改编译1.8，添加gitignore配置

---
+++ app/src/main/java/com/example/kanmeitu_hqf/activity/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.kanmeitu_hqf.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.kanmeitu_hqf.R;
+
+/**
+ * @author hanqf
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
